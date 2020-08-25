8af65127847b7a141b617f1afb1f4d2cb733d393
jkopen <jknoop@wice.de>
2020-08-25 12:19:17 +0200

    Updating to new login and disabling old action

--- test/actions.spec.js
+++ test/actions.spec.js
@@ -1,18 +1,21 @@
 const { expect } = require('chai');
 const { createSession } = require('./../lib/utils/wice');
 const {
-  organizations, persons, articles, configOptions, options,
-} = require('./seed/seed');
+  organizations, persons, configOptions, options,
+} = require('./seed/seed'); // articles,
 const { checkForExistingPerson, createOrUpdatePerson } = require('./../lib/actions/upsertPerson');
 const { checkForExistingOrganization, createOrUpdateOrganization } = require('./../lib/actions/upsertOrganization');
-const { checkForExistingArticle, createOrUpdateArticle } = require('./../lib/actions/upsertArticle');
+// const { checkForExistingArticle, createOrUpdateArticle } = require('./../lib/actions/upsertArticle');
 
 describe('Test actions', () => {
   it('should create or update a person', async () => {
     const cookie = await createSession(configOptions);
+
     const person = persons[2];
-    const existningPersonRowid = await checkForExistingPerson(person, cookie, options);
-    const newUser = await createOrUpdatePerson(existningPersonRowid, cookie, options, person);
+    const existingPersonRowid = await checkForExistingPerson(person, cookie, options);
+
+    const newUser = await createOrUpdatePerson(existingPersonRowid, cookie, options, person);
+
     expect(cookie).to.have.lengthOf(32);
     expect(newUser).to.be.an('object');
     expect(newUser).to.have.property('rowid');
@@ -21,20 +24,23 @@ describe('Test actions', () => {
   it('should create or update an organization', async () => {
     const cookie = await createSession(configOptions);
     const organization = organizations[1];
-    const existningOrgnizationRowid = await checkForExistingOrganization(organization, cookie, options);
-    const newOrganization = await createOrUpdateOrganization(existningOrgnizationRowid, cookie, options, organization);
+    const existingOrgnizationRowid = await checkForExistingOrganization(organization, cookie, options);
+    const newOrganization = await createOrUpdateOrganization(existingOrgnizationRowid, cookie, options, organization);
     expect(cookie).to.have.lengthOf(32);
     expect(newOrganization).to.be.an('object');
     expect(newOrganization).to.have.property('rowid');
   });
 
-  it('should create or update an article', async () => {
-    const cookie = await createSession(configOptions);
-    const article = articles[0];
-    const existningArticleRowid = await checkForExistingArticle(article, cookie, options);
-    const newArticle = await createOrUpdateArticle(existningArticleRowid, cookie, options, article);
-    expect(cookie).to.have.lengthOf(32);
-    expect(newArticle).to.be.an('object');
-    expect(newArticle).to.have.property('rowid');
-  });
+  // it('should create or update an article', async () => {
+  //   const cookie = await createSession(configOptions);
+  //   const article = articles[0];
+  //   console.log('article', article);
+  //   const existingArticleRowid = await checkForExistingArticle(article, cookie, options);
+  //   console.log('existingArticleRowid', existingArticleRowid);
+  //   const newArticle = await createOrUpdateArticle(existingArticleRowid, cookie, options, article);
+  //   console.log('newArticle', newArticle);
+  //   expect(cookie).to.have.lengthOf(32);
+  //   expect(newArticle).to.be.an('object');
+  //   expect(newArticle).to.have.property('rowid');
+  // });
 });
