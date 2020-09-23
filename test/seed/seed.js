fd7c8a2a41ff971e1e651cf594334854f889ac35
SvenHoeffler <shoeffler@wice.de>
2020-09-23 10:38:12 +0200

    Updated tests

--- test/seed/seed.js
+++ test/seed/seed.js
@@ -15,79 +15,31 @@ const options = {
   },
 };
 
-const organizations = [{
-  body: {
-    name: 'Company Ltd.',
-    email: 'info@company.com',
+const organization = {
+  meta: {
+    recordUid: '368125',
   },
-},
-{
-  body: {
-    meta: {
-      recordUid: '368125',
-    },
-    data: {
-      name: 'Test GmbH',
-      email: 'info@testgmbh.com',
-    },
+  data: {
+    name: 'Test GmbH',
+    email: 'info@testgmbh.com',
   },
-},
-{
-  body: {
-    name: 'Travel Mates',
-    email: 'info@travelmates.com',
-  },
-},
-];
-
-const persons = [{
-  body: {
-    name: 'Brown',
-    firstname: 'Adam',
-  },
-},
-{
-  body: {
-    name: 'Doe',
-    firstname: 'John',
-  },
-},
-{
-  body: {
-    meta: {
-      recordUid: '433610',
-    },
-    data: {
-      name: 'Kolarovv',
-      firstname: 'Sebastian',
-      email: 'kolarov@mail.com',
-    },
-  },
-},
-];
-
-const articles = [{
-  body: {
-    description: 'Arduino Nano',
-  },
-},
-{
-  body: {
-    description: 'Raspberry Pi',
-  },
-},
-];
-
-module.exports = {
-  configOptions,
-  persons,
-  organizations,
-  articles,
 };
+
+const person = {
+  meta: {
+    recordUid: '433610',
+  },
+  data: {
+    name: 'Kolarovv',
+    firstname: 'Sebastian',
+    email: 'kolarov@mail.com',
+  },
+};
+
+
 module.exports = {
-  options,
   configOptions,
-  persons,
-  organizations,
-  articles,
+  person,
+  organization,
+  options,
 };
